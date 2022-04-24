addded9a6be3e6ab8a1eef9b9ac20009008136fd
pravoslavzilka <62677181+pravoslavzilka@users.noreply.github.com>
2022-04-24 09:30:32 +0200

    Final changes, optimizing

--- flask_part.py
+++ flask_part.py
@@ -5,131 +5,114 @@ from models import Website
 from database import db_session
 from flask_cors import CORS, cross_origin
 
+
+# creation of Flask app + allowing Cross Origin
 app = Flask(__name__)
 CORS(app)
 
 
+# testing page, nothing to see...
 @app.route("/")
 def home_page():
-    return "<form action='/get_para/' method='post'> <input type='text' name='json-data' /> <input type='submit' /> </form>"
+    return "<form action='/get_test/' method='post'> <input type='text' name='json-data' /> <input type='submit' /> " \
+           "</form> "
 
 
+# testing page for accepting POST requests from a form
 @app.route("/get_test/", methods=["POST"])
 def get_test():
-    record = json.loads(request.data)
-    print(record)
-    return jsonify(record)
+    return main_api_connection(request.form["json-data"])
 
 
+# extension API route function returning status of website's paragraphs (GET version)
 @app.route("/get_para_get/<para>/")
 def get_paragraph(para):
-    data = json.loads(para)
-    website = Website.query.filter(Website.url == data["url_address"]).first()
-    if website:
-        return {"sequence": website.paragraph_sequence.split(",")}
-
-    documents = data["text"]
-    total_negative_sentences = []
-    responses = []
-    for doc in documents:
-        status, negative_sentences = sentiment_analysis(doc)
-        responses.append(status)
-        if len(total_negative_sentences) < 21:
-            total_negative_sentences += negative_sentences
-
-    count_of_para = {i: responses.count(i) for i in responses}
-    print(count_of_para)
-
-    new_website = Website(data["url_address"], data["title"])
-    try:
-        new_website.negative_columns = ".".join(total_negative_sentences)
-    except:
-        pass
-    new_website.paragraph_sequence = ",".join(responses)
-    try:
-        new_website.negative_count = count_of_para["negative"]
-    except:
-        pass
-
-    try:
-        new_website.positive_count = count_of_para["positive"]
-    except:
-        pass
-
-    db_session.add(new_website)
-    db_session.commit()
-
-    print(responses)
-    return {"sequence": responses}
+    return main_api_connection(para)
 
 
+# extension API route function returning status of website's paragraphs (POST version)
 @app.route("/get_para/", methods=["POST"])
 def get_paragraph_post():
-    data = json.loads(request.data)
+    return main_api_connection(request.data)
+
+
+# Main API linking extension with Azure API
+def main_api_connection(json_data):
+    # loads data from the extension
+    data = json.loads(json_data)
     website = Website.query.filter(Website.url == data["url_address"]).first()
     if website:
         return {"sequence": website.paragraph_sequence.split(",")}
 
+    # access data from JSON request from the extension
     documents = data["text"]
+    # negative sentences to show in dashboard
     total_negative_sentences = []
     responses = []
+    # for each paragraph in the request...
     for doc in documents:
         status, negative_sentences = sentiment_analysis(doc)
         responses.append(status)
-        if len(total_negative_sentences) < 21:
+        # limiting number of negative sentences
+        if len(total_negative_sentences) + len(negative_sentences) < 15:
             total_negative_sentences += negative_sentences
 
+    # counting total positive, negative and neutral sentences
     count_of_para = {i: responses.count(i) for i in responses}
-    print(count_of_para)
+    print("\n")
+    print(f"Counts of paragraphs with each status: {count_of_para}")
 
+    # creating new row in a database
     new_website = Website(data["url_address"], data["title"])
-    try:
-        new_website.negative_columns = ".".join(total_negative_sentences)
-    except:
-        pass
+    new_website.negative_columns = ".".join(total_negative_sentences)
     new_website.paragraph_sequence = ",".join(responses)
-    try:
-        new_website.negative_count = count_of_para["negative"]
-    except:
-        pass
 
-    try:
-        new_website.positive_count = count_of_para["positive"]
-    except:
-        pass
+    new_website.negative_count = count_of_para["negative"] if "negative" in count_of_para else 0
+    new_website.positive_count = count_of_para["positive"] if "positive" in count_of_para else 0
 
+    # write out changes in database
     db_session.add(new_website)
     db_session.commit()
 
+    print(f"Final sequence: {responses}")
     return {"sequence": responses}
 
 
+# Access data of single website
 @app.route("/get-website-info/<url_address>")
 def get_website_info(url_address):
     web = Website.query.filter(Website.url == url_address).first()
     if web:
-        return {"url_address": web.url, "title": web.title, "negative": web.negative_columns.split("."), "negative_count": web.negative_count, "positive": web.positive_count}
+        return {
+            "url_address": web.url,
+            "title": web.title,
+            "negative": web.negative_columns.split("."),
+            "negative_count": web.negative_count,
+            "positive": web.positive_count
+        }
     return {"info":"Nothing was found"}
 
 
-@app.route("/get_single_para/", methods=["POST"])
-def get_single_paragraph_post():
-    para = request.form["json-data"]
+# check status of single paragraph (for user's inputs)
+@app.route("/get_single_para/<para>/", methods=["GET"])
+def get_single_paragraph_post(para):
     data = json.loads(para)
-
     status, negative_sentences = sentiment_analysis(data["text"])
     return status
 
 
+# get data about whole searching activity
 @app.route("/get-daily-stats")
 def get_daily_stats():
     websites = Website.query.all()
     sequence_of_websites = {}
+
+    # transforming data to JSON
     for index, website in enumerate(websites):
         sequence_of_websites[index] = {
             "url_address": website.url,
-            "title" : website.title,
-            "negative": website.negative_columns,
+            "title": website.title,
+            "negative": website.negative_columns.split("."),
             "positive_count": website.positive_count,
             "negative_count": website.negative_count
         }
