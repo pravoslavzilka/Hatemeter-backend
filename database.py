9a8005b5576e87846472b78ba559858594b86f08
pravoslavzilka <62677181+pravoslavzilka@users.noreply.github.com>
2022-04-24 09:32:05 +0200

    Optimizing

--- database.py
+++ database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-#engine = create_engine('mysql+pymysql://doadmin:EvfcNp3HA-xXqXcK@db-mysql-fra1-95866-do-user-9796066-0.b.db.ondigitalocean.com:25060/defaultdb', pool_recycle=3600)
+
+# usage SQLlite only for demo purposes
+# connection with database, creating connection session object to connect with database
 engine = create_engine('sqlite:///database.db', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
@@ -11,6 +13,7 @@ Base = declarative_base()
 Base.query = db_session.query_property()
 
 
+# function to initialize tables in database
 def init_db():
     import models
     Base.metadata.create_all(bind=engine)
